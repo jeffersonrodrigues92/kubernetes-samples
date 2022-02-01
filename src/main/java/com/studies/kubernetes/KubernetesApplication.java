502e888135e7f743590c69af0744d33226144d91
Jefferson <jefferson.comff@hotmail.com>
2022-02-01 09:14:27 -0300

    first commit

---
+++ src/main/java/com/studies/kubernetes/KubernetesApplication.java
@@ -0,0 +1,12 @@
+package com.studies.kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/studies/kubernetes/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.studies.kubernetes.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok("Hello World");
+    }
+}
